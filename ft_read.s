7bc40453cc8f2fbfd9883fa7eb232c8ff8ad536d
effervescinn <32834764+effervescinn@users.noreply.github.com>
2021-04-15 16:21:33 +0300

    read и write с обработкой ошибок

--- ft_read.s
+++ ft_read.s
@@ -1,9 +1,18 @@
+extern      ___error
 global		_ft_read
 section		.text
-
 _ft_read:
-			mov		rax, 0x2000003
+            push        rcx
+			mov		    rax, 0x2000003
             syscall
-            jmp     .end
+            jc          .error
+            jmp         .end
+.error:
+            mov         rcx, rax
+            call        ___error
+            mov         [rax], rcx
+            mov         rax, -1
+            jmp         .end
 .end:
+            pop         rcx
             ret
--- ft_write.s
+++ ft_write.s
@@ -1,13 +1,18 @@
 extern      ___error
 global		_ft_write
 section		.text
-
 _ft_write:
+            push        rcx
 			mov		    rax, 0x2000004
             syscall
-            jf      .error
-            jmp     .end
-.error:     
-            mov     
+            jc          .error
+            jmp         .end
+.error:
+            mov         rcx, rax
+            call        ___error
+            mov         [rax], rcx
+            mov         rax, -1
+            jmp         .end
 .end:
+            pop         rcx
             ret
